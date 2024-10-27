e9ed869c01fe5c000d7846b6d3516eb9020c4ef2
mkalitka <mikolaj.kalitka@proton.me>
2024-10-28 00:50:27 +0100

    test: added Flask routes tests

--- python_logo/routes.py
+++ python_logo/routes.py
@@ -1,25 +1,25 @@
-from flask import Blueprint, Response, render_template, request
+from flask import Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
-def index() -> str:
+def index() -> tuple[str, int]:
     """Renders the index page.
 
     Returns:
         str: The rendered index page.
     """
-    return render_template("index.html")
+    return render_template("index.html"), 200
 
 
 @main.route("/", methods=["POST"])
-def index_post() -> Response:
+def index_post() -> tuple[str, int]:
     """Handles form submission and prints the user's code.
 
     Returns:
         Response: A response object with status 204.
     """
-    user_code = request.form["code"]
+    user_code = request.form.get("code")
     print(user_code)
-    return Response(status=204)
+    return "", 204
--- tests/test_logo.py
+++
@@ -1,5 +0,0 @@
-from python_logo import logo
-
-
-def test_print_logo():
-    assert logo() == "logo"
