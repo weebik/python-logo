255c457ae7987ecdd9dfbebee142b9e31107431e
YarynaRachkevych1 <rachkevychyaryna@gmail.com>
2024-10-27 13:05:43 +0100

    fix: resolve code quality issues from tests

--- python_logo/__init__.py
+++ python_logo/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+
 from .routes import main
+
 
 def logo() -> str:
     """Returns logo string"""
     return "logo"
 
 def create_app():
+    """Create and configure the Flask application.
+
+    :return: Flask application instance.
+    """
     app = Flask(__name__)
     app.register_blueprint(main)
 
--- python_logo/routes.py
+++ python_logo/routes.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=["GET"])
-def index_get():
+def index_get() -> str:
+    """GET endpoint for homepage.
+
+    :return: Rendered template with homepage.
+    """
     return render_template("index.html")
 
 @main.route('/', methods=["POST"])
-def index_post():
+def index_post() -> str:
+    """POST endpoint for homepage.
+
+    :return: Redirect response to homepage.
+    """
     code = request.form["code"]
     print(code)
-    return render_template("index.html")
+    return redirect("/")
