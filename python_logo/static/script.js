efb00890c154f3333be55cb16349225ab9c071a5
weebik <krystian.samaa@gmail.com>
2024-10-27 17:18:17 +0100

    fix: resolve issue with resetting textarea

--- python_logo/static/script.js
+++ python_logo/static/script.js
@@ -1,40 +1,40 @@
 // function to resize textarea
-var element = document.getElementById('resizable');
-var resizer = document.createElement('div');
-resizer.className = 'resizer';
+var element = document.getElementById("resizable");
+var resizer = document.createElement("div");
+resizer.className = "resizer";
 
 element.appendChild(resizer);
-resizer.addEventListener('mousedown', initResize, false);
+resizer.addEventListener("mousedown", initResize, false);
 
 function initResize(e) {
-   window.addEventListener('mousemove', startResizing, false);
-   window.addEventListener('mouseup', stopResizing, false);
+  window.addEventListener("mousemove", startResizing, false);
+  window.addEventListener("mouseup", stopResizing, false);
 }
 
 function startResizing(e) {
-   element.style.width = (e.clientX - element.offsetLeft) + 'px';
+  element.style.width = e.clientX - element.offsetLeft + "px";
 }
 
 function stopResizing(e) {
-    window.removeEventListener('mousemove', startResizing, false);
-    window.removeEventListener('mouseup', stopResizing, false);
+  window.removeEventListener("mousemove", startResizing, false);
+  window.removeEventListener("mouseup", stopResizing, false);
 }
 
 // Sending code from text area to backend server
 function sendCode() {
-   const code = document.getElementById('codeTextarea').value;
+  const code = document.getElementById("codeTextarea").value;
 
-   fetch("/", {
-       method: "POST",
-       headers: {
-           "Content-Type": "application/x-www-form-urlencoded"
-       },
-       body: new URLSearchParams({ code: code })
-   })
-   .then(response => {
-       if (response.redirected) {
-           window.location.href = response.url;
-       }
-   })
-   .catch(error => console.error("Error:", error));
+  fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ code: code }),
+  })
+    .then((response) => {
+      if (response.redirected) {
+        window.location.href = response.url;
+      }
+    })
+    .catch((error) => console.error("Error:", error));
 }
