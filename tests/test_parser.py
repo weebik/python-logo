b47f709e4be495d3540413db75a471e07bd3bda6
Magda Tyton <magdaeelna@gmail.com>
2024-11-11 19:45:07 +0100

    fix: change variable name in tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ from python_logo.parser import parse_logo
 def test_forward():
     forward_input = "forward 100"
     forward_alias_input = "fd 100"
-    forward_response = {"commands": [{"command": "forward", "value": 100}]}
+    forward_response = {"commands": [{"name": "forward", "value": 100}]}
     assert parse_logo(forward_input) == forward_response
     assert parse_logo(forward_alias_input) == forward_response
 
@@ -12,7 +12,7 @@ def test_forward():
 def test_backward():
     backward_input = "backward 100"
     backward_alias_input = "bk 100"
-    backward_response = {"commands": [{"command": "backward", "value": 100}]}
+    backward_response = {"commands": [{"name": "backward", "value": 100}]}
     assert parse_logo(backward_input) == backward_response
     assert parse_logo(backward_alias_input) == backward_response
 
@@ -20,7 +20,7 @@ def test_backward():
 def test_left():
     left_input = "left 90"
     left_alias_input = "lt 90"
-    left_response = {"commands": [{"command": "left", "value": 90}]}
+    left_response = {"commands": [{"name": "left", "value": 90}]}
     assert parse_logo(left_input) == left_response
     assert parse_logo(left_alias_input) == left_response
 
@@ -28,7 +28,7 @@ def test_left():
 def test_right():
     right_input = "right 90"
     right_alias_input = "rt 90"
-    right_response = {"commands": [{"command": "right", "value": 90}]}
+    right_response = {"commands": [{"name": "right", "value": 90}]}
     assert parse_logo(right_input) == right_response
     assert parse_logo(right_alias_input) == right_response
 
@@ -36,7 +36,7 @@ def test_right():
 def test_showturtle():
     showturtle_input = "showturtle"
     showturtle_alias_input = "st"
-    showturtle_response = {"commands": [{"command": "showturtle"}]}
+    showturtle_response = {"commands": [{"name": "showturtle"}]}
     assert parse_logo(showturtle_input) == showturtle_response
     assert parse_logo(showturtle_alias_input) == showturtle_response
 
@@ -44,7 +44,7 @@ def test_showturtle():
 def test_hideturtle():
     hideturtle_input = "hideturtle"
     hideturtle_alias_input = "ht"
-    hideturtle_response = {"commands": [{"command": "hideturtle"}]}
+    hideturtle_response = {"commands": [{"name": "hideturtle"}]}
     assert parse_logo(hideturtle_input) == hideturtle_response
     assert parse_logo(hideturtle_alias_input) == hideturtle_response
 
@@ -52,7 +52,7 @@ def test_hideturtle():
 def test_penup():
     penup_input = "penup"
     penup_alias_input = "pu"
-    penup_response = {"commands": [{"command": "penup"}]}
+    penup_response = {"commands": [{"name": "penup"}]}
     assert parse_logo(penup_input) == penup_response
     assert parse_logo(penup_alias_input) == penup_response
 
@@ -60,7 +60,7 @@ def test_penup():
 def test_pendown():
     pendown_input = "pendown"
     pendown_alias_input = "pd"
-    pendown_response = {"commands": [{"command": "pendown"}]}
+    pendown_response = {"commands": [{"name": "pendown"}]}
     assert parse_logo(pendown_input) == pendown_response
     assert parse_logo(pendown_alias_input) == pendown_response
 
@@ -86,22 +86,22 @@ def test_all():
     """
     logo_response = {
         "commands": [
-            {"command": "forward", "value": 20},
-            {"command": "forward", "value": 30},
-            {"command": "backward", "value": 40},
-            {"command": "backward", "value": 50},
-            {"command": "left", "value": 60},
-            {"command": "left", "value": 70},
-            {"command": "right", "value": 80},
-            {"command": "right", "value": 90},
-            {"command": "hideturtle"},
-            {"command": "hideturtle"},
-            {"command": "showturtle"},
-            {"command": "showturtle"},
-            {"command": "penup"},
-            {"command": "penup"},
-            {"command": "pendown"},
-            {"command": "pendown"},
+            {"name": "forward", "value": 20},
+            {"name": "forward", "value": 30},
+            {"name": "backward", "value": 40},
+            {"name": "backward", "value": 50},
+            {"name": "left", "value": 60},
+            {"name": "left", "value": 70},
+            {"name": "right", "value": 80},
+            {"name": "right", "value": 90},
+            {"name": "hideturtle"},
+            {"name": "hideturtle"},
+            {"name": "showturtle"},
+            {"name": "showturtle"},
+            {"name": "penup"},
+            {"name": "penup"},
+            {"name": "pendown"},
+            {"name": "pendown"},
         ]
     }
     assert parse_logo(logo_input) == logo_response
