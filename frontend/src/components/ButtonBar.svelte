bd545a8313f0b343dfd4cc7bcb2974ef21bbd5fd
mkalitka <mikolaj.kalitka@proton.me>
2024-12-05 02:20:41 +0100

    feat: add import and export code functionality

--- frontend/src/components/ButtonBar.svelte
+++ frontend/src/components/ButtonBar.svelte
@@ -1,14 +1,42 @@
 <script>
   import Icon from "./Icon.svelte";
-  import { code } from "./Textarea.svelte";
+  import { getCode, setCode } from "./Textarea.svelte";
   import { runTurtle, resetTurtle } from "./Turtle.svelte";
+  import { toastSuccess } from "./Toast.svelte";
+
+  let input;
+  let files = $state();
 
   function handleRun() {
-    runTurtle(code);
+    runTurtle(getCode());
   }
 
   function handleReset() {
     resetTurtle();
+  }
+
+  // handleImport
+  $effect(() => {
+    if (files) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setCode(event.target.result);
+        toastSuccess("Code imported successfully.");
+      };
+      reader.readAsText(files[0]);
+      input.value = "";
+    }
+  });
+
+  function handleExport() {
+    const blob = new Blob([getCode().trim()], { type: "text/plain" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "logo_code.txt";
+    a.click();
+    URL.revokeObjectURL(a.href);
+    a.remove();
+    toastSuccess("Code exported successfully.");
   }
 </script>
 
@@ -30,9 +58,24 @@
     </div>
     <div class="d-flex p-0 gap-2">
       <button class="p-0 m-0" title="Import code">
-        <Icon name="importCode" />
+        <input
+          id="import-code-input"
+          type="file"
+          accept=".txt"
+          bind:files
+          bind:this={input}
+          hidden
+        />
+        <label for="import-code-input">
+          <Icon name="importCode" />
+        </label>
       </button>
-      <button class="p-0 m-0" title="Export code">
+      <button
+        class="p-0 m-0"
+        title="Export code"
+        disabled={!getCode().trim()}
+        onclick={handleExport}
+      >
         <Icon name="exportCode" />
       </button>
     </div>
@@ -45,6 +88,9 @@
     height: 60px;
     z-index: 1;
   }
+  label {
+    cursor: pointer;
+  }
   button {
     user-select: none;
     border: none;
@@ -55,14 +101,17 @@
     filter: invert(90%) sepia(3%) saturate(1480%) hue-rotate(53deg)
       brightness(113%) contrast(106%);
   }
-  button:hover {
+  button:enabled:hover {
     scale: 1.1;
     filter: invert(90%) sepia(16%) saturate(674%) hue-rotate(62deg)
       brightness(103%) contrast(96%);
   }
-  button:active {
+  button:enabled:active {
     scale: 1.05;
     filter: invert(90%) sepia(16%) saturate(674%) hue-rotate(62deg)
       brightness(113%) contrast(96%);
+  }
+  button:disabled {
+    filter: grayscale(100%);
   }
 </style>
