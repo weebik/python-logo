83aa4a42acc550f5763bf3ea0d84a0891e19870c
weebik <krystian.samaa@gmail.com>
2025-02-11 17:52:41 +0100

    chore: js code comments

--- frontend/src/storeThemes.js
+++ frontend/src/storeThemes.js
@@ -2,6 +2,12 @@ import { writable } from "svelte/store";
 
 export const storeThemes = writable("dark");
 
+/**
+ * It exports a writable store called storeThemes.
+ * This store is used to manage the theme of the application.
+ * It has two methods: toggleThemeLight and toggleThemeDark.
+ * The storeThemes store is exported so that it can be used in other parts of the application to subscribe to changes in the theme and update the theme.
+ */
 export default {
   subscribe: storeThemes.subscribe,
   toggleThemeLight: () => storeThemes.update((themeColor) => "light"),
